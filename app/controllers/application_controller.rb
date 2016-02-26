f930fc8fbae3da2c50173118c619f98d8ba17bab
Brad Richardson <brad.richardson@transware.net>
2016-02-26 16:51:52 -0600

    Change the default route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "¡Hola, mundo!"
+    render text: "¡hola, mundo!"
+  end
+
+  def goodbye
+    render text: "Goodbye, world!"
   end
 end
